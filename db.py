a06752a20187cb5da714ba5e11b2cacf0cb179a3
tatyanach <tanya982709@gmail.com>
2018-11-23 16:55:06 +0300

    add new pages

---
+++ db.py
@@ -0,0 +1,51 @@
+
+_users = {
+    'ivan': {
+        'name': 'Ivan Kapustin',
+        'age': '25',
+        'smoking': 'Yes'
+    },
+
+    'igor': {
+        'name': 'Igor Lebedev',
+        'age': '18',
+        'smoking': 'Yes'
+    },
+
+    'maria': {
+        'name': 'Maria Popova',
+        'age': '22',
+        'smoking': 'No'
+    },
+
+    'olga': {
+        'name': 'Olga kravetz',
+        'age': '19',
+        'smoking': 'Yes'
+    }
+}
+
+_user_list = []
+
+for login, user_data in _users.items():
+    _new_element = {'login': login}
+    _new_element.update(user_data)
+    _user_list.append(_new_element)
+
+
+
+
+def get_users_by_name(q):
+    results = []
+
+    for user in _user_list:
+        if q.lower() in user['name'].lower():
+            results.append(user)
+    return results
+
+
+
+def get_user(username):
+    return _users.get(username)
+
+
