a870ab28aa9e055a5c8ab5e476832dd183f8c6d1
tatyanach <43241302+tatyanach@users.noreply.github.com>
2018-11-23 16:46:56 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+import db
+
 
 app = Flask(__name__)
 
@@ -11,6 +14,14 @@ def hello():
 @app.route("/user/<username>")
 def user_page(username):
     return render_template("with_events.html", user=username)
+
+@app.route('/search')
+def search_for_person():
+    q = request.args.get('query', "")
+    users = db.get_users_by_name(q)
+    return render_template('search_results.html', q=q, users=users)
+
+
 
 app.run()
 
